0479cf3acc4db248d373132ef5026f88423aeea6
vikash pandey <vikash.pandey@aiab.in>
2023-10-15 14:29:53 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import "../src/assets/scss/Structure.scss";
 import { Provider } from "react-redux";
 import store from "./store.js";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
